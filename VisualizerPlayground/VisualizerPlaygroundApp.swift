d2232b57223a90e2146bf919bd5fa06371133619
Matt Pfeiffer <mattpfeiffer54@gmail.com>
2020-12-29 18:18:59 -0600

    Initial Commit

---
+++ VisualizerPlayground/VisualizerPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisualizerPlaygroundApp.swift
+//  VisualizerPlayground
+//
+//  Created by Matt Pfeiffer on 12/29/20.
+//
+
+import SwiftUI
+
+@main
+struct VisualizerPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
